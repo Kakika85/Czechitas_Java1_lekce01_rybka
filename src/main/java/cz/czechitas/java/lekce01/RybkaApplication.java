8b3f747960dc8a55c7f286d9a2077ece27fc63c7
Frolíková Karolina <frolikova.karolina@gmail.com>
2024-01-20 10:27:21 +0100

    rybička plave do ctverce a vraci se na původni souradnice

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -8,32 +8,56 @@ import cz.czechitas.java.playground.Rybka;
 import javafx.scene.paint.Color;
 
 public class RybkaApplication extends CzechitasGameApplication {
-  private Rybka rybka;
-  private Kamen kamen;
+    private Rybka rybka;
+    private Kamen kamen;
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+    private int kamenX = 200;
+    private int kamenY = 200;
 
-  @Override
-  protected void initSettings(GameSettings settings) {
-    settings.setNative(true);
-    settings.setWidth(800);
-    settings.setHeight(600);
-    settings.setTitle("Rybka Renata");
-    settings.setVersion("1.0");
-  }
+    private int rybkaX = 100;
+    private int rybkaY = 100;
 
-  @Override
-  protected void initGame() {
-    FXGL.getGameScene().setBackgroundColor(Color.AQUA);
-    super.initGame();
-  }
+    private int delkaKroku = 10;
+    private int steps = (int) ((kamenX - rybkaX) * 2 / delkaKroku);
 
-  @Override
-  protected void onStart() {
-    kamen = new Kamen(200, 200);
-    rybka = new Rybka(100, 100);
-  }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setNative(true);
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Rybka Renata");
+        settings.setVersion("1.0");
+    }
+
+    @Override
+    protected void initGame() {
+        FXGL.getGameScene().setBackgroundColor(Color.AQUA);
+        super.initGame();
+    }
+
+    @Override
+    protected void onStart() {
+        kamen = new Kamen(kamenX, kamenY);
+        rybka = new Rybka(rybkaX, rybkaY);
+        gaming();
+    }
+
+    protected void gaming() {
+        for (int x = 0; x <= steps; x++) {
+            rybka.plavatVpravo();
+        }
+        for (int x = 0; x <= steps; x++) {
+            rybka.plavatDolu();
+        }
+        for (int x = 0; x <= steps; x++) {
+            rybka.plavatVlevo();
+        }
+        for (int x = 0; x <= steps; x++) {
+            rybka.plavatNahoru();
+        }
+    }
 }
